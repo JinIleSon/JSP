28f957ca8e072cb8e7719dfbd86e1594360fcba3
thswlsdlf <sy7521012@naver.com>
2025-07-30 14:54:31 +0900

    add ch06

---
+++ ch06/src/main/java/dto/FileDTO.java
@@ -0,0 +1,13 @@
+package dto;
+
+public class FileDTO {
+	
+	private int no;
+	private String title;
+	private String name;
+	private String oName;
+	private String sName;
+	private String rdate;
+	
+	
+}
